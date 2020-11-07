5559bff494a022950aef1df5177ae7546ae5d25a
Tragoedie <72974188+Tragoedie@users.noreply.github.com>
2020-11-08 01:01:59 +0700

    add_MassVote

---
+++ MassVote.py
@@ -0,0 +1,31 @@
+def MassVote(N, Votes):
+    summ_votes = 0
+    for i in range(N):
+        summ_votes += Votes[i]
+    percent_votes = []
+    for j in range(N):
+        percent = Votes[j] / summ_votes * 100
+        percent_votes.append(percent)
+    max_votes = percent_votes[0]
+    for x in range(1, N):
+        if percent_votes[x] > max_votes:
+            max_votes = percent_votes[x]
+    count = 0
+    winner = 0
+    result = ''
+    for y in range(N):
+        if max_votes == percent_votes[y]:
+            count += 1
+            winner = y + 1
+    if count > 1:
+        result = "no winner"
+    else:
+        if max_votes > 50:
+            result = "majority winner " + str(winner)
+        else:
+            result = "minority winner " + str(winner)
+    return result
+
+# N = 4
+# Votes = [111, 111, 110, 110]
+# print(MassVote(N, Votes))
