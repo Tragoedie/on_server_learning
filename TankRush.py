5be9c3cb0d23f3a4a00da3f4388762cdc52bb1b6
Tragoedie <72974188+Tragoedie@users.noreply.github.com>
2020-11-10 19:20:17 +0700

    add_TankRush

---
+++ TankRush.py
@@ -0,0 +1,42 @@
+def TankRush(H1, W1, S1, H2, W2, S2):
+    massive = []
+    map = []
+    tank = []
+    for a in range(len(S1)):
+        if S1[a] != ' ':
+            massive.append(S1[a])
+        else:
+            map.append(massive)
+            massive = []
+    map.append(massive)
+    massive = []
+    for b in range(len(S2)):
+        if S2[b] != ' ':
+            massive.append(S2[b])
+        else:
+            tank.append(massive)
+            massive = []
+    tank.append(massive)
+    print(map)
+    print(tank)
+    xchange = False
+    for i in range(0, H1-H2+1):
+        for j in range(0, W1-W2+1):
+            if map[i][j] == tank[0][0]:
+                coordinate_x = i
+                coordinate_y = j
+                xchange = True
+                for x in range(coordinate_x, coordinate_x+H2):
+                    for y in range(coordinate_y, coordinate_y+W2):
+                        if map[x][y] != tank[x-coordinate_x][y-coordinate_y]:
+                            xchange = False
+                            break
+                    if xchange is False:
+                        break
+    if xchange is True:
+        return True
+    else:
+        return False
+
+
+# print(TankRush(3, 4, '1234 2345 0987', 2, 2, '34 00'))
