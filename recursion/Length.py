3f31d53debba2d8cc85b6b0de4fbc20b6d9b464e
Tragoedie <72974188+Tragoedie@users.noreply.github.com>
2020-11-20 16:29:06 +0700

    change_lenght

--- recursion/Length.py
+++ recursion/Length.py
@@ -1,10 +1,11 @@
 def lenght(line):
-    if len(line) == 1:
-        return 1
+    if line == []:
+        return 0
     else:
         line.pop(0)
         return 1 + lenght(line)
 
 
-# print(lenght(['1', '2', '3', '4', '5']))
-# print(lenght(['1']))
+print(lenght(['1', '2', '3', '4', '5']))
+print(lenght(['1']))
+print(lenght([]))
