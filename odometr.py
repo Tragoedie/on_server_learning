44cc090c38c1c73b3fe4c68b667fcd57e2d822b8
Tragoedie <72974188+Tragoedie@users.noreply.github.com>
2020-12-27 20:21:01 +0700

    change_tree

--- odometr.py
+++
@@ -1,9 +0,0 @@
-def odometer(oksana):
-    if len(oksana) % 2 != 0 or len(oksana) < 2 or oksana[0] < 0 or oksana[1] <= 0:
-        return -1
-    S = oksana[0] * oksana[1]
-    for i in range(2, len(oksana), 2):
-        if oksana[i] < 0 or oksana[i+1] <= oksana[i-1]:
-            return -1
-        S += oksana[i]*(oksana[i+1]-oksana[i-1])
-    return S
