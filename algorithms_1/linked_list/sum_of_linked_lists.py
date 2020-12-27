f450b7e23e732a81dfcace2c08d29f94dc54e50d
Tragoedie <72974188+Tragoedie@users.noreply.github.com>
2020-12-27 23:44:22 +0700

    add_sum_of_linked_lists

---
+++ algorithms_1/linked_list/sum_of_linked_lists.py
@@ -0,0 +1,40 @@
+from on_server_learning.algorithms_1.linked_list.work_linked_list import Node
+from on_server_learning.algorithms_1.linked_list.work_linked_list import LinkedList
+
+
+def massive_of_value(s_list):
+    node = s_list.head
+    massive = []
+    while node is not None:
+        massive.append(node.value)
+        node = node.next
+    return massive
+
+
+def sum_of_linked_list(lin_list_1, lin_list_2):
+    if lin_list_1.len() == lin_list_2.len():
+        result_massive = []
+        list_1 = massive_of_value(lin_list_1)
+        list_2 = massive_of_value(lin_list_2)
+        for i in range(len(list_1)):
+            result_massive.append(list_1[i] + list_2[i])
+        return result_massive
+
+
+s_list_1 = LinkedList()
+s_list_1.add_in_tail(Node(1))
+s_list_1.add_in_tail(Node(1))
+s_list_1.add_in_tail(Node(1))
+
+
+s_list_2 = LinkedList()
+s_list_2.add_in_tail(Node(1))
+s_list_2.add_in_tail(Node(1))
+s_list_2.add_in_tail(Node(1))
+
+
+summ = sum_of_linked_list(s_list_1, s_list_2)
+if summ is None:
+    print('The lengths of linked lists are not equal')
+else:
+    print(summ)
