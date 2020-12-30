fa85afe0f7c5b15367b32ee5cd5a0b42c9324c35
Tragoedie <72974188+Tragoedie@users.noreply.github.com>
2020-12-30 16:07:48 +0700

    add_linked_list_2

--- algorithms_1/linked_list/sum_of_linked_lists.py
+++ algorithms_1/linked_list/sum_of_linked_lists.py
@@ -13,12 +13,13 @@ def massive_of_value(s_list):
 
 def sum_of_linked_list(lin_list_1, lin_list_2):
     if lin_list_1.len() == lin_list_2.len():
-        result_massive = LinkedList()
+        result_list = LinkedList()
         list_1 = massive_of_value(lin_list_1)
         list_2 = massive_of_value(lin_list_2)
         for i in range(len(list_1)):
-            result_massive.add_in_tail(Node(list_1[i] + list_2[i]))
-        return result_massive
+            result_list.add_in_tail(Node(list_1[i] + list_2[i]))
+        return result_list
+    return None
 
 
 s_list_1 = LinkedList()
@@ -26,12 +27,10 @@ s_list_1.add_in_tail(Node('1'))
 s_list_1.add_in_tail(Node(2))
 s_list_1.add_in_tail(Node('3'))
 
-
 s_list_2 = LinkedList()
 s_list_2.add_in_tail(Node('1'))
 s_list_2.add_in_tail(Node(2))
 s_list_2.add_in_tail(Node('3'))
-
 
 summ = sum_of_linked_list(s_list_1, s_list_2)
 if summ is None:
---
+++ algorithms_1/linked_list/work_linked_list_2.py
@@ -0,0 +1,97 @@
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.prev = None
+        self.next = None
+
+
+class LinkedList2:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+            item.prev = None
+            item.next = None
+        else:
+            self.tail.next = item
+            item.prev = self.tail
+        self.tail = item
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        node = self.head
+        massive = []
+        while node is not None:
+            if node.value == val:
+                massive.append(node)
+            node = node.next
+        return massive
+
+    def delete(self, val, all=False):
+        node = self.head
+        if node is None:
+            return
+        while node is not None:
+            if node.value == val:
+                if self.head == self.tail:
+                    self.head = self.tail = None
+                elif node.prev is None:
+                    self.head = node.next
+                    node.next.prev = None
+                elif self.tail == node:
+                    self.tail = node.prev
+                    node.prev.next = None
+                else:
+                    node.next.prev = node.prev
+                    node.prev.next = node.next
+                if all is False:
+                    return
+            node = node.next
+
+    def clean(self):
+        self.head = None
+        self.tail = None
+
+    def len(self):
+        length = 0
+        node = self.head
+        while node is not None:
+            node = node.next
+            length += 1
+        return length
+
+    def insert(self, afterNode, newNode):
+        if self.head is None:
+            self.head = self.tail = newNode
+        elif afterNode is None:
+            self.tail.next = newNode
+            newNode = self.tail
+        else:
+            newNode.next = afterNode.next
+            afterNode.next = newNode
+            newNode.prev = afterNode
+            if afterNode == self.tail:
+                self.tail = newNode
+                return
+            newNode.next.prev = newNode
+
+    def add_in_head(self, newNode):
+        self.head.prev = newNode
+        newNode.next = self.head
+        newNode = self.head
+
+    def print_all_nodes(self):
+        node = self.head
+        while node is not None:
+            print(node.value)
+            node = node.next
