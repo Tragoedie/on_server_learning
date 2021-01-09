5251cc3492933058f2efaf8f16b27695ed8be8f8
Tragoedie <72974188+Tragoedie@users.noreply.github.com>
2021-01-09 23:47:53 +0700

    chenge_rotation_queue

--- algorithms_1/stack/rotation_queue.py
+++ algorithms_1/stack/rotation_queue.py
@@ -1,22 +1,20 @@
 from on_server_learning.algorithms_1.stack.queue_linked_list import Queue
 
 
-def rotation_against_the_clock(queue, N):
-    if queue.size() <= 1:
-        return
-    while N > 0:
-        val = queue.dequeue()
-        queue.enqueue(val)
-        N -= 1
-
-
-def rotation_clockwise(queue, N):
+def rotation_against_the_clock(queue, N, change=False):
     if queue.size() <= 1:
         return
     while N > queue.size():
         N = N - queue.size()
-    result = queue.size() - N
-    rotation_against_the_clock(queue, result)
+    if change is True:
+        N = queue.size() - N
+    while N > 0:
+        queue.enqueue(queue.dequeue())
+        N -= 1
+
+
+def rotation_clockwise(queue, N):
+    rotation_against_the_clock(queue, N, True)
 
 
 qu = Queue()
@@ -28,7 +26,7 @@ qu.enqueue(5)
 qu.enqueue(6)
 qu.enqueue(7)
 qu.enqueue(8)
-rotation_clockwise(qu, 5)
+rotation_against_the_clock(qu, 1)
 print(qu.head.value)
 print(qu.tail.value)
 qu.print_all_nodes()
