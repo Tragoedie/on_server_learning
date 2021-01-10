a666af1e9c800963396d73af4c2b144df2cd3a3f
Tragoedie <72974188+Tragoedie@users.noreply.github.com>
2021-01-11 02:11:25 +0700

    change_rotation

--- algorithms_1/stack_queue_deque/rotation_queue.py
+++ algorithms_1/stack_queue_deque/rotation_queue.py
@@ -4,8 +4,9 @@ from on_server_learning.algorithms_1.stack_queue_deque.queue_linked_list import 
 def rotation_against_the_clock(queue, N, change=False):
     if queue.size() <= 1:
         return
-    while N > queue.size():
-        N = N - queue.size()
+    N = N % queue.size()
+    # while N > queue.size():
+        # N = N - queue.size()
     if change is True:
         N = queue.size() - N
     while N > 0:
@@ -26,7 +27,7 @@ qu.enqueue(5)
 qu.enqueue(6)
 qu.enqueue(7)
 qu.enqueue(8)
-rotation_against_the_clock(qu, 1)
+rotation_against_the_clock(qu, 17)
 print(qu.head.value)
 print(qu.tail.value)
 qu.print_all_nodes()
