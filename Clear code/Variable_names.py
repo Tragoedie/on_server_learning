eb592b31aeff48738459bbcf88b9bcd108851b5e
Tragoedie <moyatragoedia@gmail.com>
2021-05-14 19:25:41 +0700

    add_variable_names

---
+++ Clear code/Variable_names.py
@@ -0,0 +1,12 @@
+list_number - arrayOfRandomNumbers (array_of_random_numbers) \\ массив случайных значениц
+sum - sumOfAllNumbers (sum_of_all_numbers) \\ сумма всех значений массива
+b - arrayOfNodesMatchingTag (array_of_nodes_matching_tag) \\массив узлов с подходящим тегом
+counter - counterOfNodes (counter_of_nodes)\\ счетчик количества узлов
+list_1 - firstFile (first_file) \\ первый файл
+sum_1 - sumOfAllNumberFirstFile (sum_of_all_number_first_file) \\ сумма чисел первого файла
+potok - threadForMessageReceiving (thread_for_message_receiving)\\ поток для функции приема сообщений сервера
+max_1 - maxNumber (max_number) \\ максимальное число, max_2 = secondMaxNumber (second_max_number) \\ второе максимальное число
+user - arrayOfCompletedAndAllTasks (array_of_completed_and_all_tasks) \\ массив колличества заданий (всего и выполненно)
+user_count - counterOfUser (counter_of_user) \\ счетчик пользователей
+st_2 - stackOfNumbers (stack_of_numbers) \\стек для хранения чисел
+line - arrayFromString (array_from_string) \\ создание массива из строки
