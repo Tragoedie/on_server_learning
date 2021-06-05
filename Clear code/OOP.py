be84341a1478d574a036441a3a3c83aecb8fee83
Tragoedie <moyatragoedia@gmail.com>
2021-06-06 00:02:39 +0700

    add_OOP

---
+++ Clear code/OOP.py
@@ -0,0 +1,5 @@
+3.1.    1) lst_symbols = LinkedList(strWord) -> lst_symbols = LinkedList.FromString(strWord) // LinkedList - создание связного списка из строки
+        2) word_tree = SimpleTree(strText) -> word_tree = SimpleTree.SplitTextToWords(strText) // SimpleTree - создание дерева из строки
+        3) heap_from_array = HeapSort(array) -> heap = HeapSort.CreateFromArray(array) // HeapSort - создание кучи из массива
+
+3.2.    Weapon -> WeaponFactory
