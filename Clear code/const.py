299e9648c0218dcecba64a38adfbc1bdf006e013
Tragoedie <moyatragoedia@gmail.com>
2021-06-08 23:07:02 +0700

    add_const

---
+++ Clear code/const.py
@@ -0,0 +1,20 @@
+Константы ранее не использовались в решениях.
+
+client_1.py - были добавлены константы, вместо чисел, и даны им имена
+1) 1024  -> BUFFER_SIZE
+2) 11000 -> SERVER_PORT
+enchant.py - были добавлены константы, вместо чисел, и даны им имена
+3) 0.3 -> PHYS_ENCHANT_MULTIPLIER
+4) 0.2 -> MAG_ENCHANT_MULTIPLIER
+5) 7   -> SUCCESS_ENCHANT_CHANCE
+BastShoe.py - были добавлены константы, вместо чисел, и даны им имена
+6) 1   -> CMD_PUSH_BACK
+7) 2   -> CMD_ERASE_BACK
+8) 3   -> CMD_GET_SYMBOL
+9) 4   -> CMD_UNDO
+10) 5  -> CMD_REDO
+11) 12 -> CMD_PUSH_OR_ERASE
+ConquestCampaign.py - были добавлены константы, вместо чисел, и даны им имена
+12) 0 -> REGION_ENEMY
+13) 1 -> REGION_NEUTRAL
+14) 2 -> REGION_ALLIANCE
