155b7ea1d92bf4574e2e13d29f2dcba86f811bc4
Tragoedie <moyatragoedia@gmail.com>
2021-06-30 23:53:25 +0700

    add_comments

---
+++ Clear code/comments.py
@@ -0,0 +1,21 @@
+1) BastShoe.py
+if int(command[0]) < 1 or int(command[0]) > 5: #1 - add string, 2- substr string, 3 - undo, 4 - redo, ...
+
+2) BastShoe.py
+if last_action == 12 or len(redo_massive) == 0: # 0 - no prev command, 12 - string manipulation language
+
+3) ConquestCampaign.py
+if kingdom_s[i][j] == 1: # 0 - territory under enemy control, 1 - territory will be conquered at next step, 2 - alliance territory
+
+4) MaximumDiscount.py
+if (j + 1) % 3 == 0: # every 3-rd purchase is free
+
+5) MisterRobot.py
+while data[i] > data[i+1] or data[i+1] > data[i+2]: # a b c -> b c a
+
+6) UFO.py
+def UFO(N, data, octal): # converts decimal value to octal or hex
+
+7) the_elder_scrolls.py
+response = requests.get('https://account.elderscrollsonline.com/store?_ga='
+                        '2.55761772.613162147.1607856548-953723473.1604326764') # The Elder Scrolls eShop site
