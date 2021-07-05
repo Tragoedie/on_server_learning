d5190e8c9e8af3d9cfd8d0a89599d9cbed4653af
Tragoedie <moyatragoedia@gmail.com>
2021-07-06 00:12:50 +0700

    add_comments_two

---
+++ Clear code/comments_two.py
@@ -0,0 +1,48 @@
+1) BalancedParentheses_2.py
+#Рекурсивный перебор всех возможных комбинаций () - 1
+def BalancedParentheses_2(work_massive, index, N, open_parenthesis, close_parenthesis, massive):
+
+2) BastShoe.py
+#12 - код, обозначающий что предыдущей командой была команда добавления/удаления - 1
+if last_action == 12 or len(redo_massive) == 0:
+
+3) BigMinus.py
+#Т.к. входящие параметры были строками, то и формат выходного параметра останется таким же - 3
+work_difference = ''.join(work_difference)
+
+4) ConquestCampaign.py
+#Батальон уже высаживался на определённые поля, их обязательно нужно пометить со старта - 5
+kingdom_s[coordinate_x - 1][coordinate_y - 1] = 1
+
+5)
+#Только с удерживаемых полей возможен захват соседних территорий. С недавно захваченных полей захват соседних клеток невозможен - 5
+if kingdom_s[x][y] == 2:
+
+6) LineAnalysis.py
+#TODO - добавить возможность проверять при первом символе равным _ - 6
+if line[0] != '*':
+
+7) MassVote.py
+#Функция не возвращает данные напрямую, для возврата значений использует передаваемый во входных параметрах массив. - 1
+CalcPercentage(N, Votes, percent_votes)
+
+8) PatternUnlock.py
+#Не используйте N != len(hits), иначе программа может завершится с исключением out of range либо некорректно обработать данные - 4
+def PatternUnlock(N, hits):
+
+9) SumOfThe.py
+#TODO - убрать N из параметров и высчитывать самостоятельно либо добавить проверку на принадлежание границам массива - 6
+def SumOfThe(N, data):
+
+10) Keymaker.py
+#данный код работает дольше в виду работы с корнями и квадратами чисел, рекомендуется использование Keymaker(k) - 4
+#def Keymaker_math(k):
+# def Keymaker_math_2(k):
+
+11) UFO.py
+#TODO - добавить возможность перевода в двоичную систему счисления - 6
+def UFO(N, data, octal):
+
+12) linked_list_with_dummy.py
+#Не использовать insert для добавления в начало или конец списка, это приведёт к фатальным ошибкам - 4
+class LinkedList2_with_dummy(LinkedList2):
