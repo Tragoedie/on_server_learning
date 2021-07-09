ac1c48ee2b3375a8a442fa438221297d6d812d42
Tragoedie <moyatragoedia@gmail.com>
2021-07-09 22:53:23 +0700

    add_bad_comments

---
+++ Clear code/bad_comments.py
@@ -0,0 +1,119 @@
+1) ShopOLAP.py
+def sort_quantity_asc(item_s, amount_s):
+    change = False
+    while change is False:  # sort quantity in ascending order
+- 4
+
+2) ConquestCampaign.py
+for a in range(N):
+    for b in range(M):
+        while kingdom_s[a][b] == 0: #Сheck if kindom is free
+- 7
+
+3) SherloclValidString.py
+# print(SherlockValidString('xyz'))
+# print(SherlockValidString('xyzaa'))
+# print(SherlockValidString('xxyyy'))
+# print(SherlockValidString('xxxyy'))
+# print(SherlockValidString('xyzzz'))
+# print(SherlockValidString('xxyyza'))
+# print(SherlockValidString('xxyyzabc'))
+# print(SherlockValidString('xyxyzazaz'))
+- 12
+
+4) brain_games.py
+def main_two():
+    """Print initial load screen.
+
+   Returns:
+       basestring: user name.
+   """ - 11
+
+5) brain_calc.py
+def main():
+    """Starts Calculator game.""" - 11
+
+6) cli.py
+def welcome_user():
+    """Ask user name, and greeteing.
+
+    Returns:
+        string: The return value
+    """ - 11
+
+7) brain_even.py
+def main():
+    """Starts Even game.""" - 11
+
+8) for_brain_even.py
+def is_parity(number):
+    """Check if number is odd.
+
+   Args:
+       number (int): number to check.
+
+   Returns:
+       basestring: returns expression with result.
+   """ - 11
+
+9) for_brain_even.py
+def brain_even_logic():
+    """Even game logic.""" - 11
+
+10) for_brain_calc.py
+def brain_calc_logic():
+    """Calculator game logic.""" - 11
+
+11) for_brain_calc.py
+def generate_expression():
+    """Generate mathematical expression.
+
+   Returns:
+       basestring: returns expression with result.
+   """ - 11
+
+12) engine.py
+def ask_user_name_and_greeting():
+    """Ask user name and greeting.
+
+    Returns:
+        basestring: user name.
+    """ - 11
+
+13) engine.py
+def ask_question_and_get_answer(data_for_question):
+    """Print question in console and get user answer.
+
+    Args:
+        data_for_question (string): game question to user.
+
+    Returns:
+        basestring: user answer.
+    """ - 11
+
+14) engine.py
+def is_win_or_not(name_user, counter, correct_answer, user_answer):
+    """Check if game is finished.
+
+   Args:
+       name_user (string): user name.
+       correct_answer (string): correct answer, calculated by game.
+       user_answer (string): user's answer.
+       counter (int): counter of correct given answers.
+
+   Returns:
+       bool: is game finished?
+   """ - 11
+
+15) engine.py
+def is_right_answer(name_user, corr_answer, user_answer):
+    """Check if user answer is correct.
+
+   Args:
+       name_user (string): user name.
+       corr_answer (string): correct answer, calculated by game.
+       user_answer (string): user's answer.
+
+   Returns:
+       bool: is answer correct?
+   """ - 11
