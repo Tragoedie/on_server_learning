b0713254c88c94df396449be3a312eb2d4cbd096
golish <72974188+Tragoedie@users.noreply.github.com>
2022-11-10 20:14:03 +0700

    exercise_7

---
+++ ooap - 3/exercise_7.py
@@ -0,0 +1,9 @@
+Задание 7.
+
+Кластеры:
+1) Кластер отображения:
+    MainForm, AlarmClockForm, StopwatchForm, TimerForm, ScheduleForm, FinanceAnalysisForm
+2) Кластер взаимодействия:
+    AlarmClock, Stopwatch, Schedule, Timer, FinanceAnalysis
+3) Кластер базовых типов:
+    DateTime, SQLDataProvider
