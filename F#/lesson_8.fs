df7bcd4043993ed0e230304bb03bba317c1e9c51
golish <72974188+Tragoedie@users.noreply.github.com>
2023-04-22 20:36:28 +0700

    add_lesson_8

---
+++ F#/lesson_8.fs
@@ -0,0 +1,10 @@
+(*Даны две функции
+curry: (int * int -> int) -> int -> int ->int и uncurry: (int -> int ->int) -> int * int -> int
+curry f -- это функция g, где g x -- это функция h, где h y = f(x,y).
+uncurry g -- это функция f, где f(x,y) -- это значение h y для функции h = g x.
+Напишите их реализации.
+*)
+
+let curry f = function x y -> f(x,y)
+
+let uncurry f = function (x,y) -> f x y
