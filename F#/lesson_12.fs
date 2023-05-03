5671843c8d2c6f8068bc30c9c5b77cfcbd1e8db7
golish <72974188+Tragoedie@users.noreply.github.com>
2023-05-03 10:55:45 +0700

    add_lesson_112

---
+++ F#/lesson_12.fs
@@ -0,0 +1,32 @@
+// 34.1. Напишите функцию upto: int -> int list, которая работает так: upto n = [1; 2; ...; n].
+
+let rec upto = function
+ | 1 -> [1]
+ | n -> upto(n - 1) @ [n]
+
+(*let upto = function
+ | 1 -> [1]
+ | n -> [1 .. n]
+let result = upto 20
+printf "%A" result*)
+
+// 34.2. Напишите функцию dnto: int -> int list, которая работает так: downto n = [n; n-1; n-2; ...; 1].
+
+let rec dnto =function
+ | 1 -> [1]
+ | n when n < 1 -> []
+ | n -> n :: upto(n - 1)
+
+(*let result = dnto 20
+printf "%A" result*)
+
+// 34.3. Напишите функцию evenn: int -> int list, которая генерирует список из первых n неотрицательных чётных чисел.
+
+let rec evenn = function
+ | 1 -> [0]
+ | n when n < 0 -> []
+ | n -> evenn(n - 2) @ [n]
+
+(*let result = evenn -5
+printf "%A" result*)
+
