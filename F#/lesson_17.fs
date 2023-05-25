38f8bfef8e3e24657db54c2f5de3b77e95d124e6
golish <72974188+Tragoedie@users.noreply.github.com>
2023-05-25 19:51:20 +0700

    add_lesson_17

--- F#/lesson_17.fs
+++ F#/lesson_17.fs
@@ -1,8 +1,8 @@
 //Реализуйте стандартную функцию Map.tryFind самостоятельно в виде функции try_find.
 
 let try_find key m =
-    if Map.containsKey key m then Some(Map.find key m)
-    else None
+ if Map.containsKey key m then Some(Map.find key m)
+ else None
 (*
 let map_1 = Map.ofList [(128,"oksana"); (32,"oleg")]
 let result = try_find 128 map_1
